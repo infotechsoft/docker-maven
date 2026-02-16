2e1112529a87c18d29d7a58fffe47cbd7ff1da6d
Ray Bradley <rbradley@infotechsoft.com>
2026-02-16 12:13:21 -0500

    feat: make build script POSIX compatible

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
+
 if [ $# -lt 1 ]; then
-  echo "Usage: build.sh <mave-version> (push)?"
+  echo "Usage: build.sh <maven-version> (push)?"
   exit 1
 fi
+
 PUSH=
-if [[ "${2,,}" == "push" ]]; then
+if [ "$(printf '%s' "${2}" | tr '[:upper:]' '[:lower:]')" = "push" ]; then
   PUSH=--push
   echo "Pushing images to Docker Hub"
 else
@@ -13,24 +15,30 @@ else
 fi
 
 REPO_NAME=infotechsoft/maven
-MAVEN_VERSION=$1
-MAJOR_VERSION=$(echo $MAVEN_VERSION | cut -d . -f 1)
+MAVEN_VERSION="$1"
+MAJOR_VERSION=$(echo "$MAVEN_VERSION" | cut -d . -f 1)
+JAVA_VERSIONS="${JAVA_VERSIONS:-8 11 17 21 25}"
 
-BASE_IMAGES="$(< base-images.txt)"
-for BASE_IMAGE in $BASE_IMAGES; do
-  BASE_TAG=jdk-$(echo $BASE_IMAGE | cut -d ':' -f 2)
+for JAVA_VERSION in $JAVA_VERSIONS; do
+  BASE_TAG="jdk-$JAVA_VERSION"
+  BASE_IMAGE="infotechsoft/java:$JAVA_VERSION"
 
   echo "Building Maven ${MAVEN_VERSION} on ${BASE_TAG}"
   docker build \
-    --build-arg BASE_IMAGE=$BASE_IMAGE \
-    --build-arg MAVEN_VERSION=$MAVEN_VERSION \
-    ${PUSH} \
-    --tag $REPO_NAME:$MAVEN_VERSION-$BASE_TAG \
-    --tag $REPO_NAME:$MAJOR_VERSION-$BASE_TAG \
+    --build-arg BASE_IMAGE="$BASE_IMAGE" \
+    --build-arg MAVEN_VERSION="$MAVEN_VERSION" \
+    --pull \
+    ${PUSH:+"$PUSH"} \
+    --tag "$REPO_NAME:$MAVEN_VERSION-$BASE_TAG" \
+    --tag "$REPO_NAME:$MAJOR_VERSION-$BASE_TAG" \
     --provenance=true \
     --sbom=true \
     .
     
-  echo "Generating CVE report for $REPO_NAME:$MAJOR_VERSION-$BASE_TAG"
-  docker scout cves --format markdown $REPO_NAME:$MAJOR_VERSION-$BASE_TAG > reports/maven-$MAJOR_VERSION-$BASE_TAG-cves.md
+  if docker scout >/dev/null 2>&1 ; then
+    echo "Generating CVE report for $REPO_NAME:$MAJOR_VERSION-$BASE_TAG"
+    docker scout cves --format markdown "$REPO_NAME:$MAJOR_VERSION-$BASE_TAG" > reports/maven-$MAJOR_VERSION-$BASE_TAG-cves.md
+  else
+    echo "Docker scout not installed, skipping CVE report..."
+  fi
 done
